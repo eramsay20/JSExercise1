5300e1c8d7e574b3dd0bbd263760845289912cca
eramsay20 <eramsay10@gmail.com>
2020-05-15 10:30:40 -0400

    Update exercise-one.js

--- exercise-one.js
+++ exercise-one.js
@@ -14,7 +14,7 @@ Stretch Requirements:
 1. Given four variables, x, y, even, and odd, where x and y are positive numbers and even and odd start at 0; count how many numbers are even or odd between x and y.*/
 
 // STEP 1:
-
+/*
 function swapNumbers(x,y) {
 
     if(x == y) {
@@ -47,9 +47,48 @@ function swapNumbers(x,y) {
     }
      swapNumbers(15,12);
   // STEP 1 COMPLETE (ABOVE)  
+*/
 
   //STEP 2:
+/*
+let x = 1;
+let y = 12;
+let z = "";
 
+while (x <= y) {
+    if(y % x !== 0){
+        x++;
+    }
+    else{
+    z = z + x + " ";
+    x++;
+    }
+}
+console.log(z);
+document.write(`the factors of Y (${y}) are: ${z}`);
+*/
+
+let x = 3;
+let y = 40;
+let z = "";
+let count = 1;
+
+while (count <= y) {
+    if (count==0){
+        count++;
+    }
+    else if(count % x !== 0){
+        count++;
+    }
+    else{
+    z = z + count + " ";
+    count++;
+    }
+}
+console.log(z);
+document.write(`Every value divisible by X (${x}) between the positive integers 0 and Y (${y}) are: ${z}`);
+
+/*
 //STEP 2 COMPLETE ABOVE
 
 //STEP 3:
@@ -76,3 +115,53 @@ function swapNumbers(x,y) {
             break;
         }
 //STEP 3 COMPLETE ABOVE
+*/
+
+// STRETCH REQUIREMENT // 
+// Stretch Requirements:
+// 1. Given four variables, x, y, even, and odd, where x and y are positive numbers 
+// and even and odd start at 0; count how many numbers are even or odd between x and y.*
+
+let x = 3;
+let y = 40;
+let odd = 0;
+let even = 0;
+
+function evenOddCount (x, y) {
+
+    if (y > x) {
+        x = x+1;
+        while (x < y) { 
+            if(x % 2 > 0){
+                odd++;
+                x++;
+            }
+
+            else{
+                even++;
+                x++;
+            }
+        }
+    }
+    else if (x > y) {
+
+        let odd = 0;
+        let even = 0;
+        y=y+1;
+        while (y < x) {
+            if(x % 2 > 0){
+                odd++;
+                y++;
+            }
+
+            else{
+                even++;
+                y++;
+            }
+        }
+    }
+
+}
+evenOddCount(5, 10);
+
+document.write(`Even count = ${even}, while the Odd count = ${odd}`);
